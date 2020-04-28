1ee17ed6a3709937a8777b849d6d644045d67fe0
Mattias Andersson <neonithe@gmail.com>
2020-04-28 22:22:33 +0200

    Person, tests not done.

---
+++ src/main/java/se/lexicon/model/Person.java
@@ -0,0 +1,44 @@
+package se.lexicon.model;
+/**
+ Create Person class in package model.
+ a. Required fields are personId (int and final), firstName and lastName (String).
+ b. Make a constructor that can build the object.
+ c. Create needed getters and setters.
+ d. Unit test with Junit.
+ e. Commit changes.
+ **/
+public class Person {
+
+    private int personId;
+    private String firstName;
+    private String lastName;
+
+    //Constructor
+    public Person(int personId, String firstName, String lastName) {
+        setPersonId(personId);
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    /** Getters **/
+    public int getPersonId() {
+        return personId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    /** Setters **/
+    public void setPersonId(int personId) {
+        this.personId = this.personId;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/se/lexicon/model/PersonTest.java
@@ -0,0 +1,25 @@
+package se.lexicon.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static se.lexicon.model.Person.*;
+
+public class PersonTest {
+
+    private Person test;
+
+    @Before
+    public void add_values() throws Exception{
+        test = new Person(1,"TestFirstName", "TestLastName");
+    }
+
+    @Test
+    public void return_right_field(){
+
+        assertEquals(1, test.getPersonId());
+        assertEquals("TestFirstName", test.getFirstName());
+        assertEquals("TestLastName", test.getLastName());
+    }
+
+}
